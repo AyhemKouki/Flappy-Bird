af4477c5d692a89750adc14b736490f8dd52d7e6
AyhemKouki <ayhemkouki542@gmail.com>
2025-04-02 22:28:49 +0200

    bird

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.init()
 clock = pygame.time.Clock()
 
 screen_width , screen_height = 500 , 650
+gravity = 0.5
+jump_strength = -8
 
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("flappy bird")
@@ -12,6 +14,45 @@ pygame.display.set_caption("flappy bird")
 bg_img = pygame.transform.scale2x(pygame.image.load("imgs/bg.png"))
 base_img = pygame.image.load("imgs/base.png").convert_alpha()
 pipe_img = pygame.image.load("imgs/pipe.png").convert_alpha()
+bird_imgs = [pygame.image.load(f"imgs/bird{i+1}.png").convert_alpha() for i in range(3)]
+
+class Bird:
+    def __init__(self):
+        self.bird_imgs = bird_imgs
+        self.index = 0
+        self.image = self.bird_imgs[self.index]
+        self.rect = self.image.get_rect(center=(100, screen_height // 2))
+        self.velocity = 0
+        self.animate_time = 0
+    
+    def animate(self):
+        self.animate_time += 1
+        if self.animate_time % 5 == 0:
+            self.index += 1
+            if self.index >= len(self.bird_imgs):
+                self.index = 0
+            self.image = self.bird_imgs[self.index]
+
+    def jump(self):
+        self.velocity = jump_strength
+
+    def move(self):
+        self.velocity += gravity
+        self.rect.y += self.velocity
+        if self.rect.bottom >= screen_height - base_img.get_height():
+            self.rect.bottom = screen_height - base_img.get_height()
+            self.velocity = 0
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.velocity = 0
+
+    def draw(self):
+        angle = min(max(self.velocity * -5, -25), 25)
+        rotated_bird = pygame.transform.rotate(self.image, angle) # Cap angle between -25 and 25 degrees
+        rotated_rect = rotated_bird.get_rect(center=self.rect.center)
+        screen.blit(rotated_bird, rotated_rect)
+
 
 class Base:
     def __init__(self , x):
@@ -50,8 +91,12 @@ class Pipe:
         self.rect.x -= 1
         self.rotated_rect.x -= 1
 
+    def collide(self):
+        return self.rect.colliderect(bird.rect) or self.rotated_rect.colliderect(bird.rect)
+
 base_list =[Base(0),Base(base_img.get_width()),Base(base_img.get_width()*2)] 
 pipe_list = []
+bird = Bird()
 
 
 PIPE_SPAWN_EVENT = pygame.USEREVENT
@@ -65,8 +110,15 @@ while True:
             sys.exit()
         if event.type == PIPE_SPAWN_EVENT:
             pipe_list.append(Pipe())
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird.jump()
 
     screen.blit(bg_img,(0,-300))
+
+    bird.draw()
+    bird.move()
+    bird.animate()
 
     for pipe in pipe_list[:]:
         pipe.draw()
